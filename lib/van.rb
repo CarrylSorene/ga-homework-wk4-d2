b283896c32634da478f261e6b37d7e4c12f27f21
Carryl Sorene <carrylsorene@gmail.com>
2015-09-02 00:21:40 +0100

    Van can release bikes back to docking station

--- lib/van.rb
+++ lib/van.rb
@@ -7,21 +7,26 @@ class Van
     @bikes = []
   end
 
+  def bike_count
+    @bikes.count
+  end
+
   def van
     raise 'Van is full' if full?
     @bikes << bike
   end
 
-  def transport bike
-    @bikes.collect bike
-  end
+  # def transport bike
+  #   @bikes.collect bike
+  # end
  
   def full?
     bike_count == @capacity 
   end
 
-
-
+  def release bike
+    @bikes.delete bike
+  end
 
   
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -9,9 +9,19 @@ describe Van do
   let(:van) {Van.new capacity: 5}
   let(:bike) { Bike.new}
 
-  it 'should transport broken bikes to garage' do
-    van.transport(bike)
-    expect(van.bike_count).to eq 1
+  # it 'should accept broken bikes' do
+  #   van(bike.break)
+  #   expect(van.bike.break).to be true
+  # end
+
+  # it 'should transport broken bikes to garage' do
+  #   van.transport(bike)
+  #   expect(van.bike_count).to eq 1
+  # end
+
+  it 'should release fixed bikes to docking station' do
+    van.release(bike)
+    expect(van.bike_count).to eq 0
   end
 
 
