00b5a871399a4a2d6b1ee41815d9775054d45afe
Carryl Sorene <carrylsorene@gmail.com>
2015-09-02 00:05:23 +0100

    Added van capacity

--- lib/van.rb
+++ lib/van.rb
@@ -1,7 +1,30 @@
-
-
 class Van
 
+  DEFAULT_CAPACITY = 5
+
+  def initialize options = {} 
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+    @bikes = []
+  end
+
+  def van
+    raise 'Van is full' if full?
+    @bikes << bike
+  end
+
  
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -6,7 +6,21 @@ describe Van do
     5.times {van(Bike.new)}
   end
 
-  
+  let(:van) {Van.new capacity: 5}
+  let(:bike) { Bike.new}
+
+  it 'should transport broken bikes to garage' do
+    van.transport(bike)
+    expect(van.bike_count).to eq 1
+  end
+
+
+
+
+
+
+
+
 
 
 end
